74fa93d4e5c3fd642dbb2451dd4da953153abe9a
davidcoats <david@getsafetycone.com>
2023-02-08 17:02:49 -0800

    Initial commit.

---
+++ source/R5T.F0104/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.F0104
+{
+	/// <summary>
+	/// Code element name (class name, method name, parameter name) functionality.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.F0104/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.F0104
+Code element name (class name, method name, parameter name) functionality.
